be5fad4969b24475eb451d7e3d2c1bb0207ff41d
lamq1216 <929343340@qq.com>
2023-08-23 14:01:03 +0800

    feat:first commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'output_rectified_image'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (f'share/{package_name}/launch', [f'launch/image_rectified.launch.py']),
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='zymouse',
+    maintainer_email='zymouse@pixmoving.net',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': ['output_rectified_image_node = output_rectified_image.undistorted_image_node:main'
+        ],
+    },
+)
